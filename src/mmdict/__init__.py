c637b6bdea89245edd2787f83db660d08dd90751
Yaroslav Shirokov <sshirokov@gmail.com>
2021-11-14 21:09:04 -0800

    Add basic read and write and tests for it

--- src/mmdict/__init__.py
+++ src/mmdict/__init__.py
@@ -1,4 +1,35 @@
 from collections.abc import MutableMapping
 
 class MultiDict(MutableMapping):
-    pass
+    def __init__(self, initial={}):
+        self.value_store = dict()
+
+        for k,v in initial.items():
+            self[k] = v
+
+    def _to_cannonical_key(self, key):
+        '''
+        Transform a supplied key into the key used to store values in
+        `self.value_store` by resolving aliases.
+        '''
+        return key
+
+    # MutableMapping protocol definitions
+    def __getitem__(self, key):
+        value_store_key = self._to_cannonical_key(key)
+        # TODO(sshirokov): Improve the failure error to not just reply with the `value_store_key` - but the passed in key
+        #                  and possibly a list of aliases it can be known by
+        return self.value_store[value_store_key]
+
+    def __setitem__(self, key, value):
+        value_store_key = self._to_cannonical_key(key)
+        self.value_store[value_store_key] = value
+
+    def __delitem__(self, key):
+        raise NotImplementedError(f"TODO(sshirokov) delete {key}")
+
+    def __iter__(self):
+        raise NotImplementedError(f"TODO(sshirokov) return generator of all keys")
+
+    def __len__(self):
+        raise NotImplementedError(f"TODO(sshirokov) return number of keys")
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,8 +2,24 @@ import unittest
 
 import mmdict
 
-class BasicTests(unittest.TestCase):
-    def test_can_instantiate_empty_MultiDict(self):
+class MultiDictBasicTests(unittest.TestCase):
+    def test__can_instantiate_empty(self):
         d = mmdict.MultiDict()
         self.assertIsNotNone(d)
 
+    def test__can_instantiate_with_data(self):
+        d = mmdict.MultiDict({"test": "ok"})
+        self.assertIsNotNone(d)
+
+    def test__can_read_by_same_key(self):
+        d = mmdict.MultiDict()
+        d["test"] = "ok"
+        self.assertEqual(d["test"], "ok")
+
+    def test__can_insert_into_empty(self):
+        d = mmdict.MultiDict()
+        d["test"] = "ok"
+
+    def test__can_tell_if_key_exists(self):
+        d = mmdict.MultiDict({"test": "ok"})
+        self.assertTrue("test" in d)
